fdb73911f94396312f9db8f324cce4847db419b6
Celeste Rock <crock2@saddleback.edu>
2024-02-05 10:32:46 -0800

    Commit of initial project files

---
+++ src/Controller.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Controller {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    
+    public void goWelcome(ActionEvent event) throws IOException {
+     root = FXMLLoader.load(getClass().getResource("welcomeScene.fxml"));
+     stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+     scene = new Scene(root);
+     stage.setScene(scene);
+     stage.show();
+    }
+    
+    public void switchToRules(ActionEvent event) throws IOException {
+     Parent root = FXMLLoader.load(getClass().getResource("gameRules.fxml"));
+     stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+     scene = new Scene(root);
+     stage.setScene(scene);
+     stage.show();
+    }
+   }
